2d19fd50785e09ec77f2b1e28b5f1db5992ea953
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2023-05-30 22:25:20 +0000

    Add a few notes and sensor options, make vignette.

--- doc/WRBcalibrates_guide.R
+++ doc/WRBcalibrates_guide.R
@@ -1,4 +1,5 @@
 ## ----setup, include = FALSE---------------------------------------------------
+# You should be modifying this vignette from the .Rmd document in the /vignettes folder, NOT the .RMD in the /doc folder.
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -7,7 +8,7 @@ library(WRBcalibrates)
 
 
 ## ----vignette building comments, eval=FALSE, include=FALSE--------------------
-#  # You should be modifying this vignette from the .Rmd document in the /vignettes folder, not the .RMD in the /doc folder.
+#  # You should be modifying this vignette from the .Rmd document in the /vignettes folder, NOT the .RMD in the /doc folder.
 #  # To have this vignette updated on the G drive, uncomment and run the following code *after* re-building the vignette using devtools::build_vignettes()
-#  # file.copy(from = paste0(getwd(), "/WRBcalibrates_guide.html"), to = "//env-fs/env-data/corp/water/Common_GW_SW/R-packages/WRBcalibrates/WRBcalibrates user guide.html", overwrite = TRUE)
+#  # file.copy(from = paste0(dirname(getwd()), "/doc/WRBcalibrates_guide.html"), to = "//env-fs/env-data/corp/water/Common_GW_SW/R-packages/WRBcalibrates/WRBcalibrates user guide.html", overwrite = TRUE)
 
