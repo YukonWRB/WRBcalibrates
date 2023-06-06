92f2a95636ce15b3e72621a71fb34342ddb4cdb7
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2023-06-06 20:14:45 +0000

    slight change to vignettes

--- doc/WRBcalibrates_guide.R
+++ doc/WRBcalibrates_guide.R
@@ -10,5 +10,5 @@ library(WRBcalibrates)
 ## ----vignette building comments, eval=FALSE, include=FALSE--------------------
 #  # You should be modifying this vignette from the .Rmd document in the /vignettes folder, NOT the .RMD in the /doc folder.
 #  # To have this vignette updated on the G drive, uncomment and run the following code *after* re-building the vignette using devtools::build_vignettes()
-#  # file.copy(from = paste0(dirname(getwd()), "/doc/WRBcalibrates_guide.html"), to = "//env-fs/env-data/corp/water/Common_GW_SW/R-packages/WRBcalibrates/WRBcalibrates user guide.html", overwrite = TRUE)
+#  # file.copy(from = paste0(dirname(getwd()), "/doc/WRBcalibrates_guide.html"), to = "//env-fs/env-data/corp/water/Common_GW_SW/R-packages/WRBcalibrates/documentation/WRBcalibrates user guide.html", overwrite = TRUE)
 
