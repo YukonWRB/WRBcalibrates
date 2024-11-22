0bd20199afaf9f4ebb76f312338e9eb569846366
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2024-11-22 05:29:32 +0000

    Add options to modify default database connection for app

--- R/calibrate.R
+++ R/calibrate.R
@@ -5,6 +5,13 @@
 #'
 #' @param host Host address. Leave default to run locally, set to "0.0.0.0" to enable others to connect. Depends on the port specified in `port` to be open on the host machine.
 #' @param port Port number (numeric). Leave default to use the default port specified in your user options. The port you specify must be open on the host machine for it to broadcast to the network.
+#' @param dbName Name of the AquaCache database. Default is "AquaCache".
+#' @param dbHost Host address of the AquaCache database. Default is pulled from the .Renviron file.
+#' @param dbPort Port number of the AquaCache database. Default is pulled from the .Renviron file.
+#' @param dbUser Username for the AquaCache database. Default is pulled from the .Renviron file.
+#' @param dbPass Password for the AquaCache database. Default is pulled from the .Renviron file.
+#' @param RLS_user Username for row-level security. Default is pulled from the .Renviron file.
+#' @param RLS_pass Password for row-level security. Default is pulled from the .Renviron file.
 #' @param browser Open the application in a browser window right away (TRUE), or in a R window (FALSE). Default is TRUE.
 #' @param display.mode The display mode for the application. Default is "normal". See `shiny::runApp()` for more information.
 #'
@@ -12,13 +19,14 @@
 #' @export
 #'
 
-calibrate <- function(host = getOption("shiny.host", "127.0.0.1"), port = getOption("shiny.port"), browser = TRUE, display.mode = "normal") {
+calibrate <- function(host = getOption("shiny.host", "127.0.0.1"), port = getOption("shiny.port"), dbName = "AquaCache", dbHost = Sys.getenv("AquaCacheHost"), dbPort = Sys.getenv("AquaCachePort"), dbUser = Sys.getenv("AquaCacheUser"), dbPass = Sys.getenv("AquaCachePass"), RLS_user = Sys.getenv("RLS_user"), RLS_pass = Sys.getenv("RLS_pass"), browser = TRUE, display.mode = "normal") {
 
 
   appDir <- system.file("app", package = "WRBcalibrates")
 
   # Load the global variables. Contains modules as well as call to pool::pool() for connection to WRB database, library calls, and loads the translations data.table.
   source(system.file("app/app_globals.R", package = "WRBcalibrates"))
+  calibrate_globals(dbName = dbName, dbHost = dbHost, dbPort = dbPort, dbUser = dbUser, dbPass = dbPass, RLS_user = RLS_user, RLS_pass = RLS_pass)
 
   if (appDir == "") {
     stop("Calibration app not found.")
